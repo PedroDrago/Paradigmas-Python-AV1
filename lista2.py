3fde4c2bb351ae588595a047b116320c7fc1a190
Drago <pedrodrago.profissional@gmail.com>
2022-09-28 22:07:43 -0300

    Implement caixa_eletronico() function

--- lista2.py
+++ lista2.py
@@ -1,5 +1,36 @@
-def caixa_eletronico():
-    return 0
+def caixa_eletronico(quantia):
+    if quantia >= 10 or quantia <= 600:
+        cem = int(quantia/100)
+        quantia = quantia%100
+
+        cinquenta = int(quantia/50)
+        quantia = quantia%50
+
+        vinte = int(quantia/20)
+        quantia = quantia%20
+
+        dez = int(quantia/10)
+        quantia = quantia%10
+
+        cinco = int(quantia/5)
+        quantia = quantia%5
+
+        um = quantia
+
+        if cem != 0:
+            print(f'{cem} nota(s) de cem')
+        if cinquenta != 0:
+         print(f'{cinquenta} nota(s) de cinquenta')
+        if vinte != 0:
+         print(f'{vinte} nota(s) de vinte')
+        if dez != 0:
+         print(f'{dez} nota(s) de dez')
+        if cinco != 0:
+         print(f'{cinco} nota(s) de cinco')
+        if um != 0:
+         print(f'{um} nota(s) de um')
+    else:
+        print('A quantia deve ser de pelo menos 10 reais e de no maximo 600 reais.')
 
 def par_ou_impar(n):
     if n%2 == 0:
@@ -52,5 +83,3 @@ def array_intercalado(l1, l2):
         l3.append(l2[c])
     
     print(l3)
-
-array_intercalado([1,3,5,7,9,11,13,15,17,19], [2,4,6,8,10,12,14,16,18,20])
